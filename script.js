851aae13f453e80a7ee5c10fc8c00c87b833d4c9
Cory Suida <itxdevcory@gmail.com>
2019-12-04 23:40:12 -0500

    submitted assignment still in progress

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
     <html>
-    <title>Random Password Generator</title> 
+    <title>Random Password Generator</title>
     <head>
     <script>
     function pwGen(min, max) {
         docvaument.write("xx");
     }
-yy(); 
-</script> 
-</head> 
-<body> 
+yy();
+</script>
+</head>
+<body>
 < h2 onclick = "passwordGen"Generate a Password</h2>
 
-    </body> 
+    </body>
     </html>
 
-     //ex.  need the html to connect with html for this   
+     //ex.  need the html to connect with html for this
     // detect clicks <div id="demo">Click here</div> see js//
 // document.getElementById('demo').onclick = function changeContent() {
 
@@ -26,16 +26,25 @@ document.getElementById('demo').style = "Color: red";
 
     //Cory Address these concerns with tutor Tues
 // Need a button: each time pushed a new password 8-128 char is generated and printed to screen
-// Req. slider 
+// Req. slider
 // function need to specify length8
 // GIVEN that a user needs a new, secure password**
 
 WHEN prompted
 for password criteria
 
+
+
 THEN a password is generated
 
-    
+var CHARACTER_SETS = [
+[true, "Numbers", "0123456789"],
+[true, "Lowercase", "abcdefghijklmnopqrstuvwxyz"],
+[false, "Uppercase", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
+[false, "ASCII symbols", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"],
+[false, "Space", " "],
+];
+
 
 
 var characters = " !"# $ % & '()*+,-./:;<=>?@[\]^_`{|}~"
@@ -43,29 +52,31 @@ var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ';"
 var numbers = "0123456789";
 var letters.numbers = "letters + numbers"
 var password = " ";
-               for (var index = 0; index < length; index++) {            // This part came directly from Wikipedia but I cannot unsee it :( 
-               var character = Math.floor(Math.random() * all.length);
-               password += all.substring(character, character + 1);
-}
+
+for (var index = 0; index < length; index++) {
+var character = Math.floor(Math.random() * all.length);
+password += all.substring(character, character + 1);
 return password;
 }
+var generateButton
+var regSymbols - [  ]
 
 // if else if for length
-// length  => 0 
+// length  => 0
 //if length <0  alert "input leg"
 
 //forEach or Char at ?? ( for a function once for each elemenrt in an array)
 
 
 //Cory REMEMBER:
-Configuration 
+Configuration
 Globals Variables
 Initialize
 Function CryptoKeyPair
 Function pwGenerate characters, length
 
 
---------------  This is a demo from Stackoverflow- use only as ReferenceError
+
 function makeid() {
     var IdLength = document.getElementById('IdLength');
     //Strip anything but 0 to 9
@@ -82,10 +93,86 @@ function makeid() {
         }
         Results.innerHTML = text;
     }
-} <
+// from dan
+
+    function passwordLengthPrompt() ;
+        var lenth - prompt(""how long
+        if (!length)) || if (isNotaN) {
+            return;
+            return length;
+
+        }
+
+        prompt for PushSubscriptionOptions
+        var includUpper = confirm *"do you want to includer UPPER"_
+    var includeLower = confirm(  do you want to includer UPPER"_  )
+var includeNumber = condfirm ("do youw ant to include")
+
+// from dan
+
+if (!include Uppter &&
+    !include lower &&
+    !include Symbol)
+    var randomUpper-randomElemenet(bigLetters(
+        generatedPassword=generpaw + randoUpper
+    ))var randomUpper - randomElemenet(bigLetters(
+    generatedPassword = generpaw + randoUpperar randomUpper - randomElemenet(littleLetter
+        passwordOptions(
+    generatedPassword = generpaw + randoUpperar randomUpper - randomElemenet(bigLetters(
+                generatedPassword = generpaw + randoUpper
+    else console.log("atleast one picked");
+    return;
+
+
+    while(generatePW.length<options.passwordLength
+        geeratedpw = genPw \random Elemtent
+        
+        )
+        passwordText.value= password
+        generateBtc.add.eventlisternerr. generatePW
+
+        getRID of any console localStorage
+}console.log so we can see it 
+//create fuciton that makes pw
+
+    function makePW(option) {
+
+
+    }
+
+call
+
+makePW(( {
+
+    include Uppercase
+    Imclude lower
+    inclSum
+function randomElement
+
+function makePassword(options)
+
+      var passwordoptions[];
+      car gneeratePW= "";
+
+      if(options.includeUppe)
+      var 
+)
+))
+//rertimrm [ass
+
+
+.anchor.apply.
+        varpasswordlenth-passwordLengthPrompt()
+        if (length << 7 || passwordLength)
+            =
 input type = "text"
 id = "IdLength"
 oninput = "makeid()"
 placeholder = "Enter 0-9" / >
-    <
-    span id = "results" > < /span>
+    <span id = "results" ></span>
+
+    append input control at end of form
+      $("<input type='text' value='' />")
+         .attr("id", "myfieldid")
+         .attr("name", "myfieldid")
+         .appendTo("#form-0");
