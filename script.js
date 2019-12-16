1a9bc485ab229801d3a418e6c9bba6dbfd1a7f27
Cory Suida <itxdevcory@gmail.com>
2019-12-16 11:45:34 -0500

    update commit to password generator

--- script.js
+++ script.js
@@ -1,164 +1,68 @@
-
+let d = new Date();
+document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
 var CHARACTER_SETS = [
-[true, "Numbers", "0123456789"],
-[true, "Lowercase", "abcdefghijklmnopqrstuvwxyz"],
-[false, "Uppercase", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
-[false, "ASCII symbols", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"],
-[false, "Space", " "],
-];
-
-const min = 8;
-const max = 128;
-
-const generate = length => {
-    let result = [];
-
-    for (let i = 0; i < length; i++) {
-        let randomNum = Math.floor((Math.random() * (max - min)) + min);
-        let charConv = String.fromCharCode(randomNum);
-        result.push(charConv);
-    }
-
-    console.log(result.join(""));
-}
-
-const length = parseInt(process.argv[2]);
-generate(length);
-
-
-
-var characters = " !"# $ % & '()*+,-./:;<=>?@[\]^_`{|}~"
-var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ';"
-var numbers = "0123456789";
-var letters.numbers = "letters + numbers"
-var password = " ";
-
-for (var index = 0; index < length; index++) {
-var character = Math.floor(Math.random() * all.length);
-password += all.substring(character, character + 1);
-return password;
-}
-var generateButton
-var regSymbols - [  ]
-
-
-
-
-function makeid() {
-    var IdLength = document.getElementById('IdLength');
-    //Strip anything but 0 to 9
-    var UserInput = IdLength.value.replace(/[^0-9.]/g, "");
-    //Update input value
-    IdLength.value = UserInput;
-    var Results = document.getElementById('results');
-    var text = "";
-    var shuffle = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    //Is input is empty?
-    if (IdLength !== '') {
-        for (var i = 0; i < IdLength.value; i++) {
-            text += shuffle.charAt(Math.floor(Math.random() * shuffle.length));
-        }
-        Results.innerHTML = text;
-    }
-    const min = 8;
-const max = 128;
-
-const generate = length => {
-    let result = [];
-}
-    for (let i = 0; i < length; i++) {
-        let randomNum = Math.floor((Math.random() * (max - min)) + min);
-        let charConv = String.fromCharCode(randomNum);
-        result.push(charConv);
-    }
-
-    console.log(result.join(""));
-}
-
-const length = parseInt(process.argv[2]);
-generate(length);
-// from dan
-
-    function passwordLengthPrompt() ;
-        var lenth - prompt(""how long
-        if (!length)) || if (isNotaN) {
-            return;
-            return length;
-
-        }
-
-        prompt for PushSubscriptionOptions
-        var includUpper = confirm *"do you want to includer UPPER"_
-    var includeLower = confirm(  do you want to includer UPPER"_  )
-var includeNumber = condfirm ("do youw ant to include")
-
-// from dan
-
-if (!include Uppter &&
-    !include lower &&
-    !include Symbol)
-    var randomUpper-randomElemenet(bigLetters(
-        generatedPassword=generpaw + randoUpper
-    ))var randomUpper - randomElemenet(bigLetters(
-    generatedPassword = generpaw + randoUpperar randomUpper - randomElemenet(littleLetter
-        passwordOptions(
-    generatedPassword = generpaw + randoUpperar randomUpper - randomElemenet(bigLetters(
-                generatedPassword = generpaw + randoUpper
-    else console.log("atleast one picked");
-    return;
-
-
-    while(generatePW.length<options.passwordLength
-        geeratedpw = genPw \random Elemtent
-
-        )
-        passwordText.value= password
-        generateBtc.add.eventlisternerr. generatePW
-
-        getRID of any console localStorage
-}console.log so we can see it
-//create fuciton that makes pw
-
-    function makePW(option) {
-
-
-    }
-
-call
-
-makePW(( {
-
-    include Uppercase
-    Imclude lower
-    inclSum
-function randomElement
-
-function makePassword(options)
-
-      var passwordoptions[];
-      car gneeratePW= "";
-
-      if(options.includeUppe)
-      var
-)
-))
-//rertimrm [ass
-
-
-.anchor.apply.
-        varpasswordlenth-passwordLengthPrompt()
-        if (length << 7 || passwordLength)
-            =
-input type = "text"
-id = "IdLength"
-oninput = "makeid()"
-placeholder = "Enter 0-9" / >
-    <span id = "results" ></span>
-
-    append input control at end of form
-      $("<input type='text' value='' />")
-         .attr("id", "myfieldid")
-         .attr("name", "myfieldid")
-         .appendTo("#form-0");
-
-         ----------------------------------------
+			[true, "Numbers", "0123456789"],
+			[true, "Lowercase", "abcdefghijklmnopqrstuvwxyz"],
+			[false, "Uppercase", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
+			[false, "ASCII symbols", "!\"#$%" + String.fromCharCode(38) + "'()*+,-./:;" + String.fromCharCode(60) + "=>?@[\\]^_`{|}~"],
+			[false, "Space", " "],
+		];
+			
+		var passwordElem   = document.getElementById("password"   );
+		var statisticsElem = document.getElementById("statistics" );
+		var copyElem       = document.getElementById("copy-button")
+		var cryptoObject    = null;
+		var currentPassword = null;
+		
+		
+		
+		/*-- Initialization --*/
+		
+		function initCharsets() {
+			function createElem(tagName, attribs) {
+				var result = document.createElement(tagName);
+				if (attribs !== undefined) {
+					for (var key in attribs)
+						result[key] = attribs[key];
+				}
+				return result;
+			}
+			
+			var container = document.querySelector("#charset tbody");
+			var endElem = document.querySelector("#charset tbody > tr:last-child");
+			CHARACTER_SETS.forEach(function(entry, i) {
+				var tr = createElem("tr");
+				var td = tr.appendChild(createElem("td"));
+				var input = td.appendChild(createElem("input", {
+					type: "checkbox",
+					checked: entry[0],
+					id: "charset-" + i}));
+				var td = tr.appendChild(createElem("td"));
+				var label = td.appendChild(createElem("label", {
+					htmlFor: "charset-" + i,
+					textContent: " " + entry[1] + " "}));
+				var small = label.appendChild(createElem("small", {
+					textContent: "(" + entry[2] + ")"}));
+				container.insertBefore(tr, endElem);
+			});
+		}
+		
+		
+		function initCrypto() {
+			var elem = document.getElementById("crypto-getrandomvalues-entropy");
+			elem.textContent = "\u2717";  // X mark
+			
+			if ("crypto" in window)
+				cryptoObject = crypto;
+			else if ("msCrypto" in window)
+				cryptoObject = msCrypto;
+			else
+				return;
+			
+			if (!("getRandomValues" in cryptoObject) || !("Uint32Array" in window) || typeof Uint32Array != "function")
+				cryptoObject = null;
+			else
+				elem.textContent = "\u2713";
+		}
+		
+		
